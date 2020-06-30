7a745ad6d7307501cb1ef1c1b41091c96e8d38fa
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2020-06-30 15:57:58 -0300

    Updates

---
+++ precache-manifest.203a0751e555ff2b04cb8e3c6105b245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce0fa135c3bb0ad206f3cae3d3e9f7a",
+    "url": "/projeto_pessoal/index.html"
+  },
+  {
+    "revision": "e1eccc65ca24616a421a",
+    "url": "/projeto_pessoal/static/css/main.1fa327cc.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/projeto_pessoal/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projeto_pessoal/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1eccc65ca24616a421a",
+    "url": "/projeto_pessoal/static/js/main.d8e7e234.chunk.js"
+  },
+  {
+    "revision": "d64f2bc3bca0996c377d",
+    "url": "/projeto_pessoal/static/js/runtime-main.d0f912b4.js"
+  },
+  {
+    "revision": "26b3904b0d4543a8cc7ddd30bd134b4e",
+    "url": "/projeto_pessoal/static/media/fotoperfil.26b3904b.jpg"
+  }
+]);
